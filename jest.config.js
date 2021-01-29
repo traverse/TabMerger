d6bb56a9ade0d33501f6286aa860841d1b98b00a
traverse <7240988+traverse@users.noreply.github.com>
2021-01-29 15:06:22 +0100

    Add stryker for mutation testing

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ const ignore_arr = [
 ];
 
 module.exports = {
-  rootDir: "../",
+  rootDir: ".",
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/tests/__mocks__/fileMock.js",
@@ -15,4 +15,5 @@ module.exports = {
   coveragePathIgnorePatterns: ignore_arr,
   verbose: true,
   coverageThreshold: { global: { statements: 95, branches: 95, functions: 95, lines: 95 } },
+  roots: ['src/', 'tests/'],
 };
